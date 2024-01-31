eee65854730a46f0f22f401eeaada183627649e2
Diego Engelhard <00356119@uca.edu.sv>
2024-01-31 11:16:00 -0600

    Add: Client footer design

--- shop-app-client/src/pages/HomePage/HomePage.jsx
+++ shop-app-client/src/pages/HomePage/HomePage.jsx
@@ -5,6 +5,7 @@ import Carousel from '../../components/Carousel/Carousel';
 import Categories from '../../components/Category/Categories/Categories';
 import Products from '../../components/Product/Products/Products';
 import Newsletter from '../../components/Newsletter/Newsletter';
+import Footer from '../../components/Footer/Footer';
 
 const HomePage = () => {
     return (
@@ -17,6 +18,8 @@ const HomePage = () => {
             <Products />
             {/* NEWSLETTER */}
             <Newsletter />
+            {/* FOOTER */}
+            <Footer />
         </>
     )
 }
